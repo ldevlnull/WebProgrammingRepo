a3426b7ae31a7a14902677f58631ab6fcc4ff2a2
ldevlnull <oleksii.ulianov@everymatrix.com>
2020-11-06 00:26:42 +0200

    [lab4_6] fixed regexp of date in lab6 and added lab7

--- lab4_6/js/jQueryPreview.js
+++ lab4_6/js/jQueryPreview.js
@@ -1,19 +1,70 @@
-const sampleText = document.getElementById('sample-text');
-
 let isBold = false;
 let isRed = false;
+let isTransparent = false;
 
-$('#btn-toggle-text').click(ev => sampleText.hidden = !sampleText.hidden);
-$('#btn-bold-text').click(ev => {
-    isBold = !isBold;
-    sampleText.style.fontWeight = (isBold) ? 'bold' : 'normal';
+const autofill_data = [
+   'dog', 'doge', 'dorian', 'donatello', 'dart',
+   'slave', 'soviet', 'sasha', 'sobaka', 'smell',
+   'america', 'anarchy', 'anti-soviet', 'atheism', 'amnesia',
+   'trump', 'tells', 'traitor', 'to', 'tie',
+   'love', 'lust', 'lukashenko', 'lier', 'lameass'
+]
+
+$(document).ready(() => {
+    const sampleText = $('#sample-text');
+
+    $('#btn-toggle-text').click(ev => sampleText.slideToggle("slow"));
+
+    $('#btn-bold-text').click(ev => {
+        isBold = !isBold;
+        sampleText.css('font-weight', (isBold) ? 'bold' : 'normal');
+    });
+
+    $('#btn-color-text').click(ev => {
+        isRed = !isRed;
+        sampleText.css('color', (isRed) ? 'red' : 'black');
+    });
+
+    const block1 = $('#sample-block1');
+    block1.click(ev => {
+        block1.animate({
+            width: 1.5 * parseInt(block1.width()) + 'px',
+            height: 1.5 * parseInt(block1.height()) + 'px'
+        }, 1000)
+    })
+
+    const block2 = $('#sample-block2');
+    block2.click(ev => {
+        console.log(block2.position())
+        block2.animate({
+            top: block2.position().top - 160,
+            left: (block2.position().left + 100)
+        }, 1000)
+    })
+
+    const block3 = $('#sample-block3');
+    block3.hover(() => block3.animate({
+            width: 500,
+            height: 500
+        }, 3000),
+        () => block3.animate({
+            width: 100,
+            height: 100
+        }, 1000)
+    );
+
+    $('#doge-btn').click(() => isTransparent
+        ? block3.fadeTo('slow', '1', 'linear', isTransparent = !isTransparent)
+        : block3.fadeTo('slow', '0.2', 'linear', isTransparent = !isTransparent)
+    );
+
+    $('#t-btn').click(() => {
+        $("tr:even").css("background-color", "blue");
+        $("tr:odd").css("background-color", "yellow");
+        $("td:last").css("color", "red");
+    });
+
+    $('#autofill').autocomplete({
+        source: autofill_data
+    });
 });
-$('#btn-color-text').click(ev => {
-    isRed = !isRed;
-    sampleText.style.color = (isRed) ? 'red' : 'black';
-} );
-$('#sample-block').click(ev => {
-    let square = $('#sample-block');
-    square.style.width = '500px';
-    square.style.height = '500px';
-})
--- lab4_6/js/validators.js
+++ lab4_6/js/validators.js
@@ -2,7 +2,7 @@ const NAME_REGEXP = new RegExp('^(([A-Z])([a-z]+)?(\'?)([a-z]+))(([ -])([A-Za-z]
 const SPACE_REDUCE_REGEXP = new RegExp(' {2,}', 'g')
 const EMAIL_REGEXP = new RegExp('^([A-Za-z])([A-Za-z._0-9]*)([A-Za-z0-9])(@)([a-z_0-9]+.){1,6}([a-z]{2,8})$')
 const PASSWORD_REGEXP = new RegExp('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$')
-const DATE_REGEXP = new RegExp('^(([0-2][0-9])|3[0-1])(\.|\/)((0[0-9])|(1[1-2]))(\.|\/)((20[0-1][0-9])|(19[2-9][0-9]))$')
+const DATE_REGEXP = new RegExp('^((?!00)([0-2][0-9])|3[0-1])(.|/)((?!00)(0[0-9])|(1[1-2]))(.|/)(((20[0-1]\\d)|(19[2-9]\\d))|(\\d{2}))$')
 const INTEREST_REGEXP = new RegExp('^\.{0,500}$')
 const PROFILE_PIR_REGEXP = new RegExp('(\.gif|\.jpg|\.png)$')
 
@@ -70,16 +70,37 @@ function checkIfPasswordMatch() {
 function handleRegister() {
     if (!checkValidation()) {
         setErrorVisibility('register', true);
+        return false;
     } else {
         setErrorVisibility('register', false);
         alert('Registration completed!');
+        return true;
     }
+}
+
+function resolveLanguages() {
+    return $('option').toArray().filter(opt => opt.selected).map(opt => opt.text).reduce((o1, o2) => o1 + ' ' + o2);
+}
+
+function fillLoginWindow() {
+    $('#l-name').append(trim($('#fname').val() + ' ' + $('#sname').val()));
+    $('#l-email').append(trim($('#email').val()));
+    $('#l-birthdate').append(trim($('#birthdate').val()));
+    $('#l-password').append(trim($('#password').val()));
+    $('#l-interests').append(trim($('#interests').val()));
+    $('#l-languages').append(resolveLanguages());
+    $('#l-profile-picture').append(trim($('#profile-pic').val()));
+
 }
 
 document.addEventListener('DOMContentLoaded', hideAllErrors)
 
-document.getElementsByClassName('register-button-login')[0].addEventListener('click', ev =>
-    document.getElementById('login-window').style.display = '')
+document.getElementsByClassName('register-button-login')[0].addEventListener('click', ev => {
+    if (handleRegister()) {
+        fillLoginWindow();
+        document.getElementById('login-window').style.display = '';
+    }
+})
 
 document.getElementById('cross-login-btn').addEventListener('click', ev =>
     document.getElementById('login-window').style.display = 'none')
