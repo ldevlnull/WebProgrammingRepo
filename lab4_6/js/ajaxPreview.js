8e93475a2a65bf4cae3de4b9fd00d8ba0137fcb3
ldevlnull <oleksii.ulianov@everymatrix.com>
2020-11-13 13:44:57 +0200

    [lab4_6] updated request

--- lab4_6/js/ajaxPreview.js
+++ lab4_6/js/ajaxPreview.js
@@ -20,3 +20,21 @@ function f(n) {
         $("#userage").val(json["data"][n].age)
     })
 }
+
+function loadTxt(txt) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        // код для IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else  {
+        // код для IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            document.getElementById('load-text').innerHTML = xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", 'resources/' + txt + ".txt",true);
+    xmlhttp.send();
+}
